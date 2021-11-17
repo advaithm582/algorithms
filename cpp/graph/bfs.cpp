b41f6579102ada32a28def91ccf7f57a87ba1edc
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2021-11-17 14:14:03 +0530

    Added BFC in C++

---
+++ cpp/graph/bfs.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file bfs.cpp
+ * @author Advaith Menon (vae7pkgv@duck.com)
+ * @brief Breadth First Search in C++
+ * @version 0.1
+ * @date 2021-11-17
+ * 
+ * @copyright Copyright (c) 2021 Advaith Menon
+ * 
+ */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+/**
+ * @brief enhanced_dbfs
+ * Graph traversing algorithm that tells the connectivity, level and
+ * shortest path between two nodes. This one uses an alternate data 
+ * structure to represent a graph ( a dict.)
+ * 
+ * @param graph The graph in below form:
+ * {{1}}
+ * @return std::vector<int> 
+ */
+// challenge: Implement it yourself ;)
