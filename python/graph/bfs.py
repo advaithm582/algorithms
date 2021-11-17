053613fa7f948e048e2bb3d712303321ef73f58c
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2021-11-17 14:05:04 +0530

    Added Enhanced Breadth First Search
    * Enhanced BFS can tell level of nodes and their parents.
    
    * Added Unittests for enhanced BFS
    
    * Updated README.md to reflect changes

--- python/graph/bfs.py
+++ python/graph/bfs.py
@@ -27,6 +27,7 @@ def basic_bfs(graph: list[tuple[int]]) -> list[int]:
         
     return visited
 
+
 def basic_dbfs(graph: dict) -> list[int]:
     """basic_dbfs
     
@@ -56,3 +57,43 @@ def basic_dbfs(graph: dict) -> list[int]:
         queue.pop(0)
         
     return visited
+
+
+def enhanced_dbfs(graph: dict) -> list[int]:
+    """enhanced_dbfs
+    
+    Graph traversing algorithm that tells the connectivity, level and
+    shortest path between two nodes. This one uses an alternate data 
+    structure to represent a graph ( a dict.)
+
+    Args:
+        graph (list[tuple[int]]): 
+            The graph in below form:
+                {1:(2,3,4)}
+
+    Returns:
+        list[int]: The visited items
+    """
+    level = {}
+    parent = {}
+    queue = []
+    
+    for i in range(1, len(graph)+1):
+        level[i] = -1 # unexplored
+        parent[i] = -1
+    
+    it1=list(graph)[0]
+    queue.append(it1)
+    level[it1] = 0
+    
+    while queue:
+        j = queue[0]
+        items = graph[j]
+        for item in items:
+            if level[item] == -1:
+                level[item] = 1 + level[j]
+                parent[item] = j
+                queue.append(item)
+        queue.pop(0)
+        
+    return {"level":level, "parents":parent}
--- python/graph/bfs_unittest.py
+++ python/graph/bfs_unittest.py
@@ -19,5 +19,24 @@ class TestBFS(unittest.TestCase):
         res2=[1, 2, 3, 4, 5, 8, 6, 7, 9, 10]
         self.assertTrue(res1==res2)
         
+    def test_adbfs(self):
+        res1 = bfs.enhanced_dbfs({
+            1:(2,3,4,),
+            2:(1,3,),
+            3:(1,2,),
+            4:(1,5,8,),
+            5:(4,6,7,),
+            6:(5,7,8,9,),
+            7:(5,6,),
+            8:(4,6,9,),
+            9:(6,8,10),
+            10:(9,),
+        })
+        op1 = {'level': {1: 0, 2: 1, 3: 1, 4: 1, 5: 2, 6: 3, 7: 3, 8: 2,
+                         9: 3, 10: 4}, 
+               'parents': {1: -1, 2: 1, 3: 1, 4: 1, 5: 4, 6: 5, 7: 5, 
+                           8: 4, 9: 8, 10: 9}}
+        self.assertEqual(res1, op1)
+        
 if __name__ == "__main__":
     unittest.main(verbosity=2)
